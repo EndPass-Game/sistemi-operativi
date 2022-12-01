12a2f907cc07552bde323393305aa26cbb800457
endpass-formatter <github-actions[bot]@users.noreply.github.com>
2022-12-01 15:14:58 +0000

    style: BOT STYLE FORMAT autocommit

--- include/macros.h
+++ include/macros.h
@@ -6,14 +6,14 @@
 #define NULL ((void *) 0)
 
 // terminal register defines
-#define ST_READY           1
-#define ST_BUSY            3
-#define ST_TRANSMITTED     5
+#define ST_READY 1
+#define ST_BUSY 3
+#define ST_TRANSMITTED 5
 
-#define CMD_ACK            1
-#define CMD_TRANSMIT       2
+#define CMD_ACK 1
+#define CMD_TRANSMIT 2
 
-#define CHAR_OFFSET        8
-#define TERM_STATUS_MASK   0xFF
+#define CHAR_OFFSET 8
+#define TERM_STATUS_MASK 0xFF
 
 #endif /* _MACROS_H */
--- include/types.h
+++ include/types.h
@@ -4,13 +4,12 @@
 typedef unsigned int u32;
 typedef unsigned int size_t;
 
-
 /* Device register type for terminals */
 typedef struct {
-	unsigned int recv_status;
-	unsigned int recv_command;
-	unsigned int transm_status;
-	unsigned int transm_command;
+    unsigned int recv_status;
+    unsigned int recv_command;
+    unsigned int transm_status;
+    unsigned int transm_command;
 } termreg_t;
 
 #endif  // _TYPES_H
