e9cf48fd67f70d5ba5905b633c9c2699a2bd5f63
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-04-15 11:09:34 +0200

    feat(mem): make coping utils faster

--- phase1/utils.c
+++ phase1/utils.c
@@ -1,14 +1,23 @@
 #include "utils.h"
 
 void memset(void *ptr, char byte, u32 size) {
-    for (u32 i = 0; i < size; i++) {
-        *((char *) ptr + i) = byte;
-    }
+  char b[] = {byte, byte, byte, byte};
+  u32 i;
+  for (i = 0; i < (size) / 4; i++) {
+    *(((int *)ptr) + i) = *((int *)b);
+  }
+  for (u32 j = 0; j < (size) % 4; j++) {
+    *((char *)ptr + j + i * 4) = byte;
+  }
 }
 
-void memcpy(void *ptr, void* ptr2, u32 size) {
-    for (u32 i = 0; i < size; i++) {
-        *((char *) ptr + i) = *((char *) ptr2 + i);
-    }
-}
 
+void memcpy(void *ptr, void *ptr2, u32 size) {
+  u32 i;
+  for (i = 0; i < (size) / 4; i++) {
+    *(((int *)ptr) + i) = *(((int *)ptr2) + i);
+  }
+  for (u32 j = 0; j < (size) % 4; j++) {
+    *((char *)ptr + j + i * 4) = *((char *)ptr2 + j + i * 4);
+  }
+}
